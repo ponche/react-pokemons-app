5774ff9b9e3e93a1b5bf975822c5a535a28b6453
Rémi PONCHE <ponche62880@gmail.com>
2022-09-03 15:33:01 +0200

    feat: affichage des Pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { POKEMONS } from './models/mock-pokemon';
 import Pokemon from './models/pokemon';
 
 const App: React.FC = () => {
-    const [pokemon] = useState<Pokemon[]>(POKEMONS);
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+    // déclaration du hook effet
+    useEffect(() => {
+        setPokemons(POKEMONS)
+    }, []);
 
     return (
         <div>
-            <h1>Pokédex</h1>
-            <p>il y a {pokemon.length} pokémons dans la pokédex</p>
+            <h1 className='center'>Pokédex</h1>
+            <p>il y a {pokemons.length} pokémons dans le pokédex</p>
+
+            <div className='container'>
+                <div className='row'>
+                    {pokemons.map(pokemon => (
+                        <div key={pokemon.name} className="card col m4">
+                            <div className='card horizontal'>
+                                <div className='card-image'>
+                                    <img src={pokemon.picture} alt={pokemon.name}></img>
+                                    <h2>{pokemon.name}</h2>
+                                </div>
+                                <div className='card-stacked'>
+                                    <p>{pokemon.name}</p>
+                                    <p><small>{pokemon.created.toString()}</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+
+                </div>
+
+            </div>
         </div>
     )
 };
