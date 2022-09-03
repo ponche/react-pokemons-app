98fb0fbec01ca6d3423504488748a1ce323e8f1d
Rémi PONCHE <ponche62880@gmail.com>
2022-09-03 20:26:17 +0200

    refactor: modification de App
    modification de App afin de rendre le code plus propre
    en utilisant le composant **pokemon-list** au lieu d'etre écrire en dure
    dans App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { POKEMONS } from './models/mock-pokemon';
-import Pokemon from './models/pokemon';
+import React from 'react';
+import PokemonList from './pages/pokemon-list';
 
 const App: React.FC = () => {
-    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
-    // déclaration du hook effet
-    useEffect(() => {
-        setPokemons(POKEMONS)
-    }, []);
+  
 
     return (
-        <div>
-            <h1 className='center'>Pokédex</h1>
-            <p>il y a {pokemons.length} pokémons dans le pokédex</p>
-
-            <div className='container'>
-                <div className='row'>
-                    {pokemons.map(pokemon => (
-                        <div key={pokemon.name} className="card col m4">
-                            <div className='card horizontal'>
-                                <div className='card-image'>
-                                    <img src={pokemon.picture} alt={pokemon.name}></img>
-                                    <h2>{pokemon.name}</h2>
-                                </div>
-                                <div className='card-stacked'>
-                                    <p>{pokemon.name}</p>
-                                    <p><small>{pokemon.created.toString()}</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-
-                </div>
-
-            </div>
-        </div>
+        <PokemonList/>
     )
 };
 
