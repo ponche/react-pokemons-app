3c2a9783de17704d274c0a8d932965451632b4f7
Rémi PONCHE <ponche62880@gmail.com>
2022-09-03 21:25:19 +0200

    feat: création du routeur

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import PokemonList from './pages/pokemon-list';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import PokemonsDetail from './pages/pokemon-detail';
+import PageNotFound from './pages/page-not-found';
 
 const App: React.FC = () => {
   
 
     return (
-        <PokemonList/>
+        <Router>
+            <div>
+                {/* La barre de navigation commun à toutes les pages */}
+                <nav>
+                    <div className="nav-wrapper teal">
+                        <Link to="/" className='brand-logo center'>Pokédex</Link>
+                    </div>
+                </nav>
+                {/* Le système de gestion des routes de notre application */}
+                <Switch>
+                    <Route exact path="/" component={PokemonList} />
+                    <Route exact path="/pokemons" component={PokemonList} />
+                    <Route path="/pokemons/:id" component={PokemonsDetail} />
+                </Switch>
+            </div>
+        </Router>
     )
 };
 
